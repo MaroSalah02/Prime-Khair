d30a571899e94706a13872eb68d6e7a48485ef36
DrBlende <omarf5925@gmail.com>
2024-05-12 21:14:03 +0200

    links donation requests to post details

--- Scripts/donor.js
+++ Scripts/donor.js
@@ -6,6 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("myelement").classList.add("hide");
   }, 1200);
 });
-function bloodDetails() {
-  window.location.href("../Donor/bloodDonationDetails.html");
+function medicalSupply() {
+  window.location.href = "../Donor/medicalSuppliesDetails.html";
+}
+function stationery() {
+  window.location.href = "../Donor/stationeryDetails.html";
+}
+function book() {
+  window.location.href = "../Donor/bookDetails.html";
+}
+function toys() {
+  window.location.href = "../Donor/toyDetails.html";
+}
+function food() {
+  window.location.href = "../Donor/foodDetails.html";
+}
+function blood() {
+  window.location.href = "../Donor/bloodDonationDetails.html";
+}
+function cloths() {
+  window.location.href = "../Donor/clothesDetails.html";
 }
